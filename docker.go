cceef556ca00ba1cfc582f7363a67170c87ab01c
Mauro Ariel Sosa <mauroariel.sosa031@gmail.com>
2021-08-29 20:42:56 -0300

    ✨ Docker status verification

---
+++ docker.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+
+	"github.com/jasonlvhit/gocron"
+)
+
+const (
+	OFF = "OFF"
+	ON  = "ON"
+)
+
+func startDockerStatusCheckJob() {
+	gocron.Every(config.VerificationInterval).Minute().Do(checkDockerStatus)
+	appendLog("[Job State]: Started cron job")
+	<-gocron.Start()
+}
+
+func checkDockerStatus() {
+	dockerState := OFF
+
+	if dockerIsRunning() {
+		emailSent = false
+		dockerState = ON
+	}
+	appendLog("[Docker State]: " + dockerState)
+
+	if dockerState == OFF && emailSent == false {
+		sendEmailReportingDockerFailure()
+	}
+}
+
+func dockerIsRunning() bool {
+	cmd := exec.Command("docker", "ps", "-a")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+
+	if err != nil {
+		return false
+	}
+	return strings.Contains(out.String(), "IMAGE") && strings.Contains(out.String(), "NAME")
+}
